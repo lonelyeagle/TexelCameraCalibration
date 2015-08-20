cb8ba6fb80ad1d36c1f4213d43428252cd792cb8
lonelyeagle <lonelyeagle@gmail.com>
2015-08-20 15:11:55 -0600

    add draw3d file

---
+++ matlabfile/draw3d.m
@@ -0,0 +1,219 @@
+clear;
+clc;
+
+fid = fopen('../flatboard/Model_black_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_black_1/Model100/Model100~/shutter_max_black_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+
+fid = fopen('../flatboard/Model_black_1/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'k');
+hold on;
+zvalue = zvalue(:);                                                                                                                                                                                                                  
+figure(2),scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k');
+hold on;
+
+
+fid = fopen('../flatboard/Model_black_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_black_2/Model100/Model100~/shutter_max_black_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_black_2/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'k');
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k');
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fid = fopen('../flatboard/Model_blue_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_blue_1/Model100/Model100~/shutter_max_blue_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_blue_1/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'b');
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b');
+
+fid = fopen('../flatboard/Model_blue_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_blue_2/Model100/Model100~/shutter_max_blue_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_blue_2/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'b');
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b');
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_brightyellow_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_brightyellow_1/Model100/Model100~/shutter_max_brightyellow_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_brightyellow_1/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', [0.8,0.8, 0]);
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663 - 0.0018, 'MarkerEdgeColor', [0.8,0.8, 0], 'MarkerFaceColor', [0.8,0.8, 0]);
+
+
+fid = fopen('../flatboard/Model_brightyellow_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_brightyellow_2/Model100/Model100~/shutter_max_brightyellow_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_brightyellow_2/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', [0.8,0.8, 0]);
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663 - 0.0018, 'MarkerEdgeColor', [0.8,0.8, 0], 'MarkerFaceColor', [0.8,0.8, 0]);
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_darkyellow_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_darkyellow_1/Model100/Model100~/shutter_max_darkyellow_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_darkyellow_1/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', [0.4,0.4, 0]);
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', [0.4,0.4, 0], 'MarkerFaceColor', [0.8,0.4, 0]);
+
+fid = fopen('../flatboard/Model_darkyellow_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_darkyellow_2/Model100/Model100~/shutter_max_darkyellow_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_darkyellow_2/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', [0.4,0.4, 0]);
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', [0.4,0.4, 0], 'MarkerFaceColor', [0.8,0.4, 0]);
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_pink_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_pink_1/Model100/Model100~/shutter_max_pink_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_pink_1/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'm');
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue+ 0.663, 'MarkerEdgeColor', 'm', 'MarkerFaceColor', 'm');
+
+fid = fopen('../flatboard/Model_pink_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_pink_2/Model100/Model100~/shutter_max_pink_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+
+fid = fopen('../flatboard/Model_pink_2/Model100/Model100~/xyz.dat', 'rb');
+[A,COUNT]=fread(fid,[3, 204*204], 'float');
+fclose(fid);
+zvalue = A(3,:);
+zvalue = reshape(zvalue(1,:), [204, 204])';
+zvalue = zvalue(31:174, 31:174);
+figure(1), surf(zvalue, 'FaceColor', 'm');
+zvalue = zvalue(:);
+figure(2), scatter3(intensity, shuttertime, zvalue + 0.663, 'MarkerEdgeColor', 'm', 'MarkerFaceColor', 'm');
--- matlabfile/rangecorrection.m
+++ matlabfile/rangecorrection.m
@@ -1,10 +1,134 @@
 clear;
 clc;
 
-fid = fopen('../RangeCorrection/Model/Model100/Model100~/xyz.dat', 'rb');
-[A,COUNT]=fread(fid,[3, 21315], 'float');
+fid = fopen('../flatboard/Model_black_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
 fclose(fid);
-xvalue = reshape(A(1,:), [145, 147]);
-yvalue = reshape(A(2,:), [145, 147]);
-zvalue = reshape(A(3,:), [145, 147]);
-mean(mean(zvalue))
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_black_1/Model100/Model100~/shutter_max_black_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity, 'k*');
+
+hold on;
+
+fid = fopen('../flatboard/Model_black_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_black_2/Model100/Model100~/shutter_max_black_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity, 'k*');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fid = fopen('../flatboard/Model_blue_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_blue_1/Model100/Model100~/shutter_max_blue_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity, 'b*');
+
+
+fid = fopen('../flatboard/Model_blue_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_blue_2/Model100/Model100~/shutter_max_blue_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity, 'b*');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_brightyellow_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_brightyellow_1/Model100/Model100~/shutter_max_brightyellow_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'*', 'color',[0.8,0.8, 0]);
+
+
+fid = fopen('../flatboard/Model_brightyellow_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_brightyellow_2/Model100/Model100~/shutter_max_brightyellow_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'*', 'color',[0.8,0.8, 0]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_darkyellow_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_darkyellow_1/Model100/Model100~/shutter_max_darkyellow_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'*', 'color',[0.4,0.4, 0]);
+
+
+fid = fopen('../flatboard/Model_darkyellow_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_darkyellow_2/Model100/Model100~/shutter_max_darkyellow_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'*', 'color',[0.4,0.4, 0]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid = fopen('../flatboard/Model_pink_1/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_pink_1/Model100/Model100~/shutter_max_pink_1.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'m*');
+
+
+fid = fopen('../flatboard/Model_pink_2/Model100/Model100~/intensity.dat', 'rb');
+[A,COUNT]=fread(fid,[1, 204*204], 'float');
+fclose(fid);
+intensity = reshape(A(1,:), [204, 204])';
+intensity = intensity(31:174, 31:174);
+intensity = intensity(:);
+
+shuttertime = load('../flatboard/Model_pink_2/Model100/Model100~/shutter_max_pink_2.txt');
+shuttertime = shuttertime(31:174, 31:174)';
+figure(2), imagesc(shuttertime);
+shuttertime = shuttertime(:);
+figure(1), plot(shuttertime, intensity,'m*');
