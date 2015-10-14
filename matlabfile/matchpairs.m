519c746d02c674fb14256646ba2fcdebcbf338da
lonelyeagle <lonelyeagle@gmail.com>
2015-10-13 19:32:17 -0600

    all update

--- matlabfile/matchpairs.m
+++ matlabfile/matchpairs.m
@@ -1,23 +1,23 @@
 clear;
 clc;
-% intensity_image = imread('../../tmp/Calibration Image 2015-06-12/BrightnessImg00007.pgm');
-% eo_image = imread('../../tmp/Calibration Image 2015-06-12/UNTRIMMED-RAW-IMAGE 0007.bmp');
-% 
-% corner1 = FindCorners(intensity_image, 'pgm');
-% 
-% pointpair = MapPoints(corner1, intensity_image, eo_image, 'pgm', 'bmp');
-% 
-% movefile('matchpoints4.txt', 'matchpoints4_new_7.txt');
+intensity_image = imread('../../tmp/calibration/BrightnessImg00010.pgm');
+eo_image = imread('../../tmp/calibration/UNTRIMMED-RAW-IMAGE 0010.bmp');
 
-file_prefix = 'matchpoints4_new_';
-data = [];
-for i = 0:7
-    tempstring = strcat(file_prefix, int2str(i), '.txt');
-    temp = load(tempstring);
-    data = [data; temp];
-end
+corner1 = FindCorners(intensity_image, 'pgm');
 
-save('matchpoints4_big_new.txt', 'data', '-ascii', '-double');
+pointpair = MapPoints(corner1, intensity_image, eo_image, 'pgm', 'bmp');
+
+movefile('matchpoints4.txt', 'matchpoints4_new_10.txt');
+
+% file_prefix = 'matchpoints4_new_';
+% data = [];
+% for i = 0:7
+%     tempstring = strcat(file_prefix, int2str(i), '.txt');
+%     temp = load(tempstring);
+%     data = [data; temp];
+% end
+% 
+% save('matchpoints4_big_new.txt', 'data', '-ascii', '-double');
 
 % ind = 1:64;
 % ind = reshape(ind, [8, 8]);
